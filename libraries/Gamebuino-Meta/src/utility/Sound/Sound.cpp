20d7c6c8c68ac6d4e8a1bb20201210bac9639191
Josh Goebel <me@joshgoebel.com>
2018-01-06 01:02:08 -0500

    remove hardware calls from sound.cpp

--- libraries/Gamebuino-Meta/src/utility/Sound/Sound.cpp
+++ libraries/Gamebuino-Meta/src/utility/Sound/Sound.cpp
@@ -45,56 +45,57 @@ Sound_Channel channels[SOUND_CHANNELS];
 Sound_Handler* handlers[SOUND_CHANNELS];
 
 bool tcIsSyncing() {
-	return TC5->COUNT16.STATUS.reg & TC_STATUS_SYNCBUSY;
+	// return TC5->COUNT16.STATUS.reg & TC_STATUS_SYNCBUSY;
+	return false;
 }
 
 void tcStart() {
 	// Enable TC
 
-	TC5->COUNT16.CTRLA.reg |= TC_CTRLA_ENABLE;
+	// TC5->COUNT16.CTRLA.reg |= TC_CTRLA_ENABLE;
 	while (tcIsSyncing());
 }
 
 void tcReset() {
 	// Reset TCx
-	TC5->COUNT16.CTRLA.reg = TC_CTRLA_SWRST;
+	// TC5->COUNT16.CTRLA.reg = TC_CTRLA_SWRST;
 	while (tcIsSyncing());
-	while (TC5->COUNT16.CTRLA.bit.SWRST);
+	// while (TC5->COUNT16.CTRLA.bit.SWRST);
 }
 
 void tcDisable() {
 	// Disable TC5
-	TC5->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE;
+	// TC5->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE;
 	while (tcIsSyncing());
 }
 
 void tcConfigure(uint32_t sampleRate) {
 	// Enable GCLK for TCC2 and TC5 (timer counter input clock)
-	GCLK->CLKCTRL.reg = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID(GCM_TC4_TC5)) ;
-	while (GCLK->STATUS.bit.SYNCBUSY);
+	// GCLK->CLKCTRL.reg = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID(GCM_TC4_TC5)) ;
+	// while (GCLK->STATUS.bit.SYNCBUSY);
 
 	tcReset();
 
 	// Set Timer counter Mode to 16 bits
-	TC5->COUNT16.CTRLA.reg |= TC_CTRLA_MODE_COUNT16;
-
-	// Set TC5 mode as match frequency
-	TC5->COUNT16.CTRLA.reg |= TC_CTRLA_WAVEGEN_MFRQ;
-
-	TC5->COUNT16.CTRLA.reg |= TC_CTRLA_PRESCALER_DIV1 | TC_CTRLA_ENABLE;
-
-	TC5->COUNT16.CC[0].reg = (uint16_t) (SystemCoreClock / sampleRate - 1);
+	// TC5->COUNT16.CTRLA.reg |= TC_CTRLA_MODE_COUNT16;
+  //
+	// // Set TC5 mode as match frequency
+	// TC5->COUNT16.CTRLA.reg |= TC_CTRLA_WAVEGEN_MFRQ;
+  //
+	// TC5->COUNT16.CTRLA.reg |= TC_CTRLA_PRESCALER_DIV1 | TC_CTRLA_ENABLE;
+  //
+	// TC5->COUNT16.CC[0].reg = (uint16_t) (SystemCoreClock / sampleRate - 1);
 	while (tcIsSyncing());
 
 	// Configure interrupt request
-	NVIC_DisableIRQ(TC5_IRQn);
-	NVIC_ClearPendingIRQ(TC5_IRQn);
-	NVIC_SetPriority(TC5_IRQn, 0);
-	NVIC_EnableIRQ(TC5_IRQn);
-
-	// Enable the TC5 interrupt request
-	TC5->COUNT16.INTENSET.bit.MC0 = 1;
-	while (tcIsSyncing());
+	// NVIC_DisableIRQ(TC5_IRQn);
+	// NVIC_ClearPendingIRQ(TC5_IRQn);
+	// NVIC_SetPriority(TC5_IRQn, 0);
+	// NVIC_EnableIRQ(TC5_IRQn);
+  //
+	// // Enable the TC5 interrupt request
+	// TC5->COUNT16.INTENSET.bit.MC0 = 1;
+	// while (tcIsSyncing());
 }
 
 void dacConfigure(void) {
@@ -321,7 +322,7 @@ uint16_t flowdown = 0;
 
 void Audio_Handler (void) {
 	if (!globalVolume || muted) {
-		TC5->COUNT16.INTFLAG.bit.MC0 = 1;
+		// TC5->COUNT16.INTFLAG.bit.MC0 = 1;
 		return;
 	}
 	int16_t output = 0;
@@ -393,7 +394,7 @@ void Audio_Handler (void) {
 			flowdown--;
 		}
 	}
-	TC5->COUNT16.INTFLAG.bit.MC0 = 1;
+	// TC5->COUNT16.INTFLAG.bit.MC0 = 1;
 }
 
 void TC5_Handler (void) __attribute__ ((alias("Audio_Handler")));
